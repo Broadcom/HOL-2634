19924a4b332f22e95d2744cdaa2edf29793b3db6
cl017888 <cristian.lamson@broadcom.com>
2025-08-06 11:57:20 -0500

    Update 2634_mount_drives.sh
    Fixed missing (

--- 2634_mount_drives.sh
+++ 2634_mount_drives.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for disk in $vdq -i | grep -o 'eui\.[a-z0-9]*'); do
+for disk in $(vdq -i | grep -o 'eui\.[a-z0-9]*'); do
   echo "Claiming ESA disk: $disk"
   esxcli vsan storagepool mount --disk $disk
 done
